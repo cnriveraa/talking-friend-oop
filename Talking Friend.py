54030651f9729d85a1236384b803957c90137dda
cnriveraa <rivera.cn18@gmail.com>
2025-02-26 17:04:56 +0800

    Update Talking Friend.py

--- Talking Friend.py
+++ Talking Friend.py
@@ -6,7 +6,6 @@ friend = pyttsx3.init()
 rate = friend.getProperty('rate')   # getting details of current speaking rate
 friend.setProperty('rate', 125)     # setting up new voice rate
 
-
 """VOLUME"""
 volume = friend.getProperty('volume')   # getting to know current volume level (min=0 and max=1)
 friend.setProperty('volume', 1)        # setting up volume level  between 0 and 1
@@ -15,7 +14,6 @@ friend.setProperty('volume', 1)        # setting up volume level  between 0 and 
 voices = friend.getProperty('voices')       # getting details of current voice
 friend.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male
 #friend.setProperty('voice', voices[1].id)   # changing index, changes voices. 1 for female
-
 
 speech = input("Type something and I will say it: ")
 friend.say(speech)
